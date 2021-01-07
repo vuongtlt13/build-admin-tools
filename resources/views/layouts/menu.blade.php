9a599ffea5d965a0017b8de08fdb5f5595a41c4b
Đỗ Quốc Vương <vuong.dq@teko.vn>
2021-01-07 16:00:46 +0700

    update code

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -1,4 +1,3 @@
-
 <li class="nav-item">
     <a href="{{route('models.index')}}" class="nav-link {{ Request::is('models*') ? 'active' : '' }}" style="padding-left: 1rem">
         <i class="fas fa-circle nav-icon"></i>
@@ -7,37 +6,9 @@
 </li>
 
 <li class="nav-item">
-    <a href="{{route('fields.index')}}" class="nav-link {{ Request::is('fields*') ? 'active' : '' }}" style="padding-left: 1rem">
-        <i class="fas fa-circle nav-icon"></i>
-        <p>Field</p>
-    </a>
-</li>
-
-<li class="nav-item">
     <a href="{{route('menus.index')}}" class="nav-link {{ Request::is('menus*') ? 'active' : '' }}" style="padding-left: 1rem">
         <i class="fas fa-circle nav-icon"></i>
         <p>Menu</p>
-    </a>
-</li>
-
-<li class="nav-item">
-    <a href="{{route('dBConfigs.index')}}" class="nav-link {{ Request::is('dBConfigs*') ? 'active' : '' }}" style="padding-left: 1rem">
-        <i class="fas fa-circle nav-icon"></i>
-        <p>DBConfig</p>
-    </a>
-</li>
-
-<li class="nav-item">
-    <a href="{{route('cRUDConfigs.index')}}" class="nav-link {{ Request::is('cRUDConfigs*') ? 'active' : '' }}" style="padding-left: 1rem">
-        <i class="fas fa-circle nav-icon"></i>
-        <p>CRUDConfig</p>
-    </a>
-</li>
-
-<li class="nav-item">
-    <a href="{{route('dTConfigs.index')}}" class="nav-link {{ Request::is('dTConfigs*') ? 'active' : '' }}" style="padding-left: 1rem">
-        <i class="fas fa-circle nav-icon"></i>
-        <p>DTConfig</p>
     </a>
 </li>
 
